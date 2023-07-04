b9f71be6e16a358205622f5eaf8f79c156a8e7f8
Anish Raj <anishraj713@gmail.com>
2023-07-04 13:59:21 +0530

    switch js add

---
+++ Basics/switch.js
@@ -0,0 +1,17 @@
+let input;
+input = "n";
+
+switch (input) {
+  case 1:
+  case "y":
+  case "yes":
+    document.write("continue.....");
+    break;
+  case 0:
+  case "n":
+  case "no":
+    document.write("End.....");
+    break;
+  default:
+    document.write("wrong input");
+}
