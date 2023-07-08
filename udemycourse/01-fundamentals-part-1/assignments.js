4f33620458e3038807d8a993f4856ff9f52d6386
Anish Raj <anishraj713@gmail.com>
2023-07-08 19:18:42 +0530

    Assignment.js added

---
+++ udemycourse/01-fundamentals-part-1/assignments.js
@@ -0,0 +1,11 @@
+// TODO: LECTURE: Values and Variables
+
+let country, continent, population;
+
+country = "India";
+continent = "Asia";
+population = 1406;
+
+console.log(country);
+console.log(continent);
+console.log(population);
