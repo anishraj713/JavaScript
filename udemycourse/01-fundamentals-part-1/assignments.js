a5a23830769b723782f2bfc4853281a7969969e3
Anish Raj <anishraj713@gmail.com>
2023-07-14 15:13:08 +0530

    assignment modified and new assignment added

--- udemycourse/01-fundamentals-part-1/assignments.js
+++ udemycourse/01-fundamentals-part-1/assignments.js
@@ -1,11 +1,65 @@
 // TODO: LECTURE: Values and Variables
+// 1. Declare variables called 'country', 'continent' and 'population' and
+// assign their values according to your own country (population in millions)
+// 2. Log their values to the console.
 
-let country, continent, population;
+// let country, continent, population;
 
+// country = "India";
+// continent = "Asia";
+// population = 1406;
+
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+// TODO: LECTURE: Data Types
+/*
+1. Declare a variable called 'isIsland' and set its value according to your
+country. The variable should hold a Boolean value. Also declare a variable
+'language', but don't assign it any value yet
+2. Log the types of 'isIsland', 'population', 'country' and 'language'
+to the console
+*/
+/*
+let isIndia, language, population, country;
+isIndia = true;
 country = "India";
-continent = "Asia";
 population = 1406;
 
+console.log(typeof isIndia);
+console.log(typeof country);
+console.log(typeof population);
+console.log(typeof language);
+*/
+
+// TODO: LECTURE: let, const and var
+/*1. Set the value of 'language' to the language spoken where you live (some
+countries have multiple languages, but just choose one)
+2. Think about which variables should be const variables (which values will never
+change, and which might change?). Then, change these variables to const.
+3. Try to change one of the changed variables now, and observe what happens
+*/
+/*
+const isIndia = true, country = "India",language = "Hindi";
+let population = 1406;
+
+console.log(isIndia);
 console.log(country);
-console.log(continent);
 console.log(population);
+console.log(language);
+*/
+
+// TODO: LECTURE: Basic Operators
+/*
+1. If your country split in half, and each half would contain half the population,
+then how many people would live in each half?
+2. Increase the population of your country by 1 and log the result to the console
+3. Finland has a population of 6 million. Does your country have more people than
+Finland?
+4. The average population of a country is 33 million people. Does your country
+have less people than the average country?
+5. Based on the variables you created, create a new variable 'description'
+which contains a string with this format: 'Portugal is in Europe, and its 11 million
+people speak portuguese'
+*/
