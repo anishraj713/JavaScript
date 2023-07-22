97c542ea4f76c07f391f4dbbde403f457918088f
Anish Raj <anishraj713@gmail.com>
2023-07-22 14:47:53 +0530

    Assignment updated

--- udemycourse/01-fundamentals-part-1/assignments.js
+++ udemycourse/01-fundamentals-part-1/assignments.js
@@ -213,3 +213,36 @@ hindi: 'Number 4'
 arabic: '5th most spoken language'
 for all other simply log 'Great language too :D
 */
+/*
+let language = "hindi";
+switch (language) {
+  case "chinese":
+  case "mandarin":
+    console.log("MOST number of native speakers!");
+    break;
+  case "spanish":
+    console.log("2nd MOST number of native speakers!");
+    break;
+  case "english":
+    console.log("3rd MOST number of native speakers!");
+    break;
+  case "hindi":
+    console.log("4th MOST number of native speakers!");
+    break;
+  case "arabic":
+    console.log("5th MOST number of native speakers!");
+    break;
+  default:
+    console.log("Great language too :D");
+}
+*/
+
+// TODO: LECTURE: The Conditional (Ternary) Operator
+/*
+1. If your country's population is greater than 33 million, use the ternary operator
+to log a string like this to the console: 'Portugal's population is above average'.
+Otherwise, simply log 'Portugal's population is below average'. Notice how only
+one word changes between these two sentences!
+2. After checking the result, change the population temporarily to 13 and then to
+130. See the different results, and set the population back to original
+*/
