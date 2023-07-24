03b3b72e45f42f6bbb7b7d48bdd2e6871f96f462
Anish Raj <anishraj713@gmail.com>
2023-07-24 14:59:07 +0530

    fundamental part 2 added

--- udemycourse/02-fundamentals-part-2/assignments.js
+++ udemycourse/02-fundamentals-part-2/assignments.js
@@ -1,4 +1,4 @@
-'use strict'; 
+"use strict";
 // TODO: LECTURE: Functions
 /*
 1. Write a function called 'describeCountry' which takes three parameters:
@@ -8,3 +8,14 @@ capital city is Helsinki'
 2. Call this function 3 times, with input data for 3 different countries. Store the
 returned values in 3 different variables, and log them to the console
 */
+
+function describeCountry(country, population, capitalCity) {
+  return `${country} has ${population} million people and its capital city is ${capitalCity} `;
+}
+
+let aboutIndia = describeCountry("india", 1402, "New Delhi");
+let aboutFinland = describeCountry("Finland", 6, "Helsinki");
+let aboutUsa = describeCountry("United States", 333, "Washington, D.C.");
+console.log(aboutIndia);
+console.log(aboutFinland);
+console.log(aboutUsa);
---
+++ udemycourse/02-fundamentals-part-2/script.js
@@ -0,0 +1,8 @@
+"use strict";
+/*
+// Basic Function
+function basic() {
+  console.log("hey! i am a function");
+}
+basic(); // Here we call the function
+*/
