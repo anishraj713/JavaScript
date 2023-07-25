41be80f5aba40d4a69f87a24fef2e78952618fce
Anish Raj <anishraj713@gmail.com>
2023-07-25 13:46:00 +0530

    Function added

--- udemycourse/02-fundamentals-part-2/assignments.js
+++ udemycourse/02-fundamentals-part-2/assignments.js
@@ -58,3 +58,29 @@ const percentageOfWorld3 = (population) => (population / 7900) * 100;
 const india = percentageOfWorld3(33);
 console.log(india.toFixed(2));
 */
+
+// TODO: LECTURE: Functions Calling Other Functions
+/*
+1. Create a function called 'describePopulation'. Use the function type you
+like the most. This function takes in two arguments: 'country' and
+'population', and returns a string like this: 'China has 1441 million people,
+which is about 18.2% of the world.'
+2. To calculate the percentage, 'describePopulation' call the
+'percentageOfWorld1' you created earlier
+3. Call 'describePopulation' with data for 3 countries of your choice
+*/
+//! Solution
+/*
+function percentageOfWorld1(population) {
+  return (population / 7900) * 100;
+}
+function describePopulation(country, population) {
+  const percentage = percentageOfWorld1(population);
+  const describeCountry = `${country} has ${population} million people, which is about ${percentage.toFixed(2)}% of the world.`;
+  return describeCountry;
+}
+
+console.log(describePopulation('India', 1402));
+console.log(describePopulation('China', 1441));
+console.log(describePopulation('USA', 332));
+*/
