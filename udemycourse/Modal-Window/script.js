5ab0021d9f684b82bdef60946d969ed6188a5372
Anish Raj <anishraj713@gmail.com>
2023-08-08 14:29:53 +0530

    Project Completed

--- udemycourse/Modal-Window/script.js
+++ udemycourse/Modal-Window/script.js
@@ -1,1 +1,27 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModel = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+
+const openModel = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+const closedModel = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener('click', openModel);
+}
+
+btnCloseModel.addEventListener('click', closedModel);
+overlay.addEventListener('click', closedModel);
+
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closedModel();
+  }
+});
